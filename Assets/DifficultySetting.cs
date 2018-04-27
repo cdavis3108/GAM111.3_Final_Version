d70704fb3a52a7ff3dabe1c7f39c968cf6193957
cdavis3108 <37386600+cdavis3108@users.noreply.github.com>
2018-04-28 00:48:35 +1000

    Difficulty Setting

---
+++ Assets/DifficultySetting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultySetting : MonoBehaviour {
+
+    private bool created = false;
+
+    public float maxHealth;
+    public float damage;
+
+    private void Awake()
+    {
+        if(!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+    public void EasyDifficulty ()
+    {
+        maxHealth = 30f;
+        damage = 5f;
+    }
+
+    public void MediumDifficulty()
+    {
+        maxHealth = 50f;
+        damage = 10f;
+    }
+
+    public void HardDifficulty()
+    {
+        maxHealth = 70f;
+        damage = 20f;
+    }
+}
