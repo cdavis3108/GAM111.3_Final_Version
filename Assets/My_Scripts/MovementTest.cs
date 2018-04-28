da461ea149b2318a4538b5afd0a9c49f6c6a4906
cdavis3108 <37386600+cdavis3108@users.noreply.github.com>
2018-04-28 22:24:51 +1000

    Started movement scripts

---
+++ Assets/My_Scripts/MovementTest.cs
@@ -0,0 +1,26 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical);
+
+        rigid.AddForce(move * speed);
+    }
+
+}
