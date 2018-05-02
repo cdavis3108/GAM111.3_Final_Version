aff492501ff4a92df3aa171918b10cd23c5ef07c
cameronM96 <37386594+cameronM96@users.noreply.github.com>
2018-05-02 12:56:45 +1000

    Added event system

---
+++ Assets/My_Scripts/CreateEnvironmentalHazards.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnvironmentalHazards : MonoBehaviour {
+
+    public bool levelOne;
+    public int numberOfHazards;
+    public GameObject hazardPrefab;
+
+    void OnEnable()
+    {
+        EventManager.OnClicked += CreateHazard;
+    }
+
+    void CreateHazard()
+    {
+        Vector3 pos = transform.position;
+
+        for (int i = 0; i < numberOfHazards; i++)
+        {
+            if (levelOne)
+            {
+                pos.x += Random.Range(-130f, 130f);
+                pos.z += Random.Range(-130f, 130f);
+            }
+            else
+            {
+                pos.x += Random.Range(-130f, 130f);
+                pos.z += Random.Range(-40f, 40f);
+            }
+            GameObject hazard = Instantiate(hazardPrefab, pos, transform.rotation);
+            hazard.transform.localScale = new Vector3(10f, 10f, 10f);
+            Destroy(hazard, 4f);
+        }
+    }
+}
---
+++ Assets/My_Scripts/EventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public delegate void ClickAction();
+    public static event ClickAction OnClicked;
+    public float eventTimer;
+    private float timer;
+
+    private void Start()
+    {
+        timer = eventTimer;
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            OnClicked();
+            timer = eventTimer;
+        }
+    }
+}
